82eaf5193f2fb7f3dc9677bf4233c60b2e43d3c7
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-10-22 22:28:00 +0300

    Update ui2py.bat

--- ui2py.bat
+++ ui2py.bat
@@ -3,5 +3,6 @@
 set uiFilename=%1
 set uiFilepath=gui\ui
 set pyFilepath=gui\templates
+set pyFilePreffix=Ui
 
-start pyuic5 %uiFilepath%\%uiFilename%.ui -o %pyFilepath%\Ui_%uiFilename%.py
+start pyuic5 %uiFilepath%\%uiFilename%.ui -o %pyFilepath%\%pyFilePreffix%_%uiFilename%.py
