ee7bd6b8e4c690366ef11e56f7a3413fa53bebc9
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-11-08 13:33:14 +0300

    Updates:
    - Saving opened earlier galler
    - Close button to close currently opened gallery
    - Update layout for left side of MainWindow

---
+++ config/GalleryConfig.py
@@ -0,0 +1,3 @@
+class GalleryConfig:
+    IMAGES_EXT: tuple = ('.jpg', '.jpeg', '.png')
+    SAVE_GALLERY_PATH = '.last_opened_gallery'
--- screens/MainWindow.py
+++ screens/MainWindow.py
@@ -5,6 +5,7 @@ from PyQt6.QtCore import pyqtSlot
 
 from GUI.Ui_MainWindow import Ui_MainWindow
 from screens.LibraryChooseDialog import LibraryChooseDialog
+from config.GalleryConfig import GalleryConfig
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     """Main application gallery window"""
@@ -29,12 +30,24 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             if dialog_result['accepted'] == True:
                 self.__open_path(dialog_result['path'])
         else:
-            # TODO: Open saved gallery path...
-            print('Open saved gallery folder path')
+            self.__open_path(self.__get_saved_dir_path())
+
+    @pyqtSlot(name='on_CloseAction_triggered')
+    def __closeAction_triggered(self) -> None:
+        """Closing opened directory and show an empty gallery"""
+        self.__set_directory_path(None)
 
     def __is_saved_dir_path(self) -> bool:
         """Returns true if the path to the gallery directory was saved at the last launch"""
+        if os.path.isfile(f'{os.getcwd()}/{GalleryConfig.SAVE_GALLERY_PATH}') and len(self.__get_saved_dir_path()) > 0:
+            return True
+
         return False
+
+    def __get_saved_dir_path(self) -> str:
+        """Returns first line from SAVE_GALLERY_PATH file"""
+        with open(f'{os.getcwd()}/{GalleryConfig.SAVE_GALLERY_PATH}', 'r') as file:
+            return file.readline()
 
     @pyqtSlot(name='on_OpenAction_triggered')
     def __openAction_triggered(self):
@@ -53,20 +66,28 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         }
 
     def __set_directory_path(self, directory_path) -> None:
-        """Set directory path and window title according to it
-
-        TODO: Save gallery path to the storage
-        """
+        """Set directory path and window title according to it"""
         self.directory_path = directory_path
         self.setWindowTitle(self.DEFAULT_WINDOW_TITLE if directory_path == None else directory_path)
 
+        with open(f'{os.getcwd()}/{GalleryConfig.SAVE_GALLERY_PATH}', 'w+') as file:
+            file.write('' if directory_path == None else directory_path)
+
+    def __chosen_dir_incorrect(self, directory_path):
+        message_box = QMessageBox(self)
+        message_box.setWindowTitle('Error')
+        message_box.setText(f'The gallery folder you specified does not look like a real existing folder\n\n{directory_path}')
+        message_box.setIcon(QMessageBox.Icon.Critical)
+        message_box.exec()
+
     def __open_path(self, directory_path: str) -> None:
         """Open provided gallery path"""
-        if (os.path.isdir(directory_path)):
-            self.__set_directory_path(directory_path)
-        else:
-            message_box = QMessageBox(self)
-            message_box.setWindowTitle('Error')
-            message_box.setText(f'The gallery folder you specified does not look like a real existing folder\n\n{directory_path}')
-            message_box.setIcon(QMessageBox.Icon.Critical)
-            message_box.exec()
+        if not os.path.isdir(directory_path):
+            self.__chosen_dir_incorrect(directory_path)
+            return
+
+        self.__set_directory_path(directory_path)
+        with os.scandir(self.directory_path) as _:
+            for entry in _:
+                if (entry.is_file and entry.name.endswith(GalleryConfig.IMAGES_EXT)):
+                    pass
